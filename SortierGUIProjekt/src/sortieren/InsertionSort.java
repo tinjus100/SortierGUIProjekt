a157a67d4232cdcdffa6b25f45da679b90c34a30
tinjus100 <61543779+tinjus100@users.noreply.github.com>
2021-12-21 21:19:42 +0100

    Add files via upload

---
+++ SortierGUIProjekt/src/sortieren/InsertionSort.java
@@ -0,0 +1,42 @@
+package sortieren;
+
+import guiProjekt.ArrayPanel;
+import guiProjekt.SortierAusgabe;
+import util.Util;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    private SortierAusgabe output;
+    public int[] a;
+
+    public InsertionSort(int[] input) {
+        a = input;
+        this.output = null;
+    }
+
+    public InsertionSort(int[] input, SortierAusgabe output) {
+        a = input;
+        this.output = output;
+        output.println("InsertionSort gestartet");
+    }
+
+    public void sort(ArrayPanel panel) {
+        if (output != null) {
+            output.println("Vor InsertionSort: " + Arrays.toString(a));
+        }
+
+        for (int i = 1; i < a.length; i++) {
+            int j = i;
+            while(j > 0 && a[j-1] > a[j]) {
+                Util.swap(a, j, j - 1);
+                panel.setArray(a);
+                j--;
+            }
+        }
+
+        if (output != null) {
+            output.println("Nach InsertionSort: " + Arrays.toString(a));
+        }
+    }
+}
