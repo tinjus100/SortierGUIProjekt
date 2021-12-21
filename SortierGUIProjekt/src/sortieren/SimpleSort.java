ccb0e789655fc8011af1f09dee66befc4735d09e
Winchester0403 <95741898+Winchester0403@users.noreply.github.com>
2021-12-21 16:21:45 +0100

    Create SimpleSort.java

---
+++ SortierGUIProjekt/src/sortieren/SimpleSort.java
@@ -0,0 +1,46 @@
+package sortieren;
+
+import guiProjekt.ArrayPanel;
+import guiProjekt.SortierAusgabe;
+import util.Util;
+
+import java.util.Arrays;
+
+public class SimpleSort {
+    private SortierAusgabe ausgabe;
+    public int[] a;
+
+    public SimpleSort(int[] input) {
+        a = input;
+        this.ausgabe = null;
+    }
+
+    public SimpleSort(int[] input, SortierAusgabe ausgabe) {
+        a = input;
+        this.ausgabe = ausgabe;
+        ausgabe.println("SimpleSort gestartet");
+    }
+
+    public void sortieren(ArrayPanel panel) {
+        // Äußere Schleife, separiert unsortierten von sortiertem Bereich
+        if (ausgabe != null) {
+            ausgabe.println("Vorher: " + Arrays.toString(a));
+        }
+
+        int k;
+        for (int i = a.length-1; i >= 1; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (a[j] >= a[i]) {
+                    k = a[i];
+                    a[i] = a[j];
+                    a[j] = k;
+                }
+                panel.setArray(a);
+            }
+        }
+
+        if (ausgabe != null) {
+            ausgabe.println("Nachher: " + Arrays.toString(a));
+        }
+    }
+}
